8840f2a80a889b6353bb262e0569c85a6fedab96
bappleyard <b.appleyard@cyprotex.com>
2014-12-04 11:31:23 +0000

    mroe tests

--- compiler/2/test_types.py
+++ compiler/2/test_types.py
@@ -26,6 +26,10 @@ exprs = [
         Call(Id('ider'), 'id', Id(0)),
         Call(Id('ider'), 'id', Id('void'))
     )),
+    Call(Id('if'), 'if', Object(('then', 'x', Id(0)), 
+                                ('else', 'x', Call(Id(0), 'add', Id(0))))),
+    Call(Id('if'), 'if', Object(('then', 'x', Call(Id(0), 'add', Id(0))), 
+                                ('else', 'x', Id(0)))),
 ]
 
 num_type = Type()
@@ -34,8 +38,12 @@ num_type.methods.append(Method('add', num_type, num_type))
 int_type = Type(Method('@int', Type(), Type()),
                 Method('add', num_type, num_type))
 void_type = Type(Method('@void', Type(), Type()))
+if_var = Var()
+if_type = Type(Method('if', Type(Method('then', void_type, if_var),
+                                 Method('else', void_type, if_var)),
+                            if_var))     
 
-env = TypeEnvironment({0: int_type, 'void': void_type},
+env = TypeEnvironment({0: int_type, 'void': void_type, 'if': if_type},
                       set([int_type, void_type]))
 for expr in exprs:
     try:
